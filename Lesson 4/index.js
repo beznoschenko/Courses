a729dcac9b098688d4c254a4f4bb2174a6213d9d
beznoschenko <anirva1994@gmail.com>
2018-02-01 22:00:50 +0200

    courses

---
+++ Lesson 4/index.js
@@ -0,0 +1,10 @@
+$(function(){
+var canvas = document.getElementById("paint");
+if (canvas.getContext){
+var ctx = canvas.getContext("2d");
+context.beginPath();
+ctx.strokeStyle = "#000";
+ctx.moveTo(50,50);
+ctx.loneTo(200,200);
+}
+})
